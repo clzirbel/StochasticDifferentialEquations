651ca45fac2c9c7e25052159072268fb20924e45
Craig L. Zirbel <zirbel@bgsu.edu>
2022-01-09 18:50:32 -0500

    Updated walk.py

--- python/walk.py
+++ python/walk.py
@@ -1,34 +1,33 @@
-# Runs a random walk.
+# walk.py simulates a simple symmetric random walk over N steps
+# Feel free to change the value of N!
+# Close the plot window to return control to the program.
 
 import random
-
-def trythis():
-    print 'Tried again twice.'
-    return
+import matplotlib.pyplot as plt
 
 def walk(N):
 
-    import random
-    import matplotlib.pyplot as plt
+    t = [0]   # list to store times
+    S = [0]   # list to store locations of the random walk
 
-    # Initialize these variables as arrays
-    t = {}
-    S = {}
+    for j in range(0,N):
+        t.append(j+1)
+        S.append(S[j] + random.choice([-1, 1]))
 
-    t[0] = 0
-    S[0] = 0
-    
-    for j in range(N):
-        t[j+1] = j + 1
-        S[j+1] = S[j] + random.choice([-1, 1])
-
-    plt.plot(t, S)
-    plt.show()
-
-    if N < 50:
-        print "N Less than 50."
+    if N <= 50:
+        # plot with lines and dots
+        plt.plot(t, S, 'b-')
+        plt.plot(t, S, 'b.')
 
     else:
-        print "N Not less than 50."
+        # just plot lines
+        plt.plot(t, S, 'b-')
+
+    plt.show()
     
     return
+
+# actually run the walk method
+walk(50)
+walk(500)
+walk(5000)
