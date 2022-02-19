c93817f535092ed4d06b2eef4f7798be639e3948
Craig L. Zirbel <zirbel@bgsu.edu>
2022-02-19 13:16:02 -0500

    Update code

--- matlab/sde_sine.m
+++ matlab/sde_sine.m
@@ -1,18 +1,20 @@
-% sde_sine.m generates outcomes of solutions of the SDE dx(t)=sin(x(t))+dB(t)
+% sde_sine.m generates outcomes of solutions of the ODE dx/dt=sin(x(t)) with noise
 
-  N=10000;            % number of steps to take
-  T=700;              % maximum time
+  T=700;              % maximum time, which is long compared to other simulations
+  N=7000000;          % number of steps to take
   h=T/N;              % time step
-  t=(0:h:T);          % t is the vector [0 1h 2h 3h ... Nh]
   sigma=1.0;          % strength of noise
 
-  X=zeros(size(t));   % place to store locations
+  t=zeros(1,N+1);     % place to store time points
+  X=zeros(1,N+1);     % place to store locations
   X(1)=0.0;           % initial location
 
   for i=1:N           % take N steps
-    X(i+1)=X(i)+sin(X(i))*h+sigma*sqrt(h)*randn;
+    t(i+1)=t(i)+h;
+    X(i+1)=X(i)+sin(X(i))*h+sigma*randn*sqrt(h);
   end;
 
+  figure(1)
   clf;
   plot(t,X);
   title('SDE with sine drift');
